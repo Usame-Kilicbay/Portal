b1888e5bffc49fc56d7d02490579266d19017d81
PapillonNOVE <muhammedkilicbay8@gmail.com>
2020-09-10 12:59:47 +0300

    Platform rotasyonu ve topun hareketi tamamlandı.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [Range(0f,1000f)]
+    [SerializeField] private float rollSpeed;
+
+    [Header("Components")]
+    [SerializeField] private SphereCollider sphereCollider;
+    [SerializeField] private Rigidbody rigidbody;
+
+	[SerializeField] private bool isTocuhedGround;
+
+	private void FixedUpdate()
+	{
+		if (!isTocuhedGround)
+		{
+			return;
+		}
+
+        Roll();
+	}
+
+	private void Roll()
+	{
+		rigidbody.MoveRotation(Quaternion.AngleAxis(Input.GetAxis("Horizontal") * Time.deltaTime * rollSpeed, Vector3.forward));
+		rigidbody.MoveRotation(Quaternion.AngleAxis(Input.GetAxis("Vertical") * Time.deltaTime * rollSpeed, Vector3.right));
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		isTocuhedGround = true;
+	}
+}
---
+++ Assets/Scripts/PlatformRotator.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class PlatformRotator : MonoBehaviour
+{
+	[Range(0f,1000f)]
+	[SerializeField] private float rotationSpeed;
+
+	[Header("Limit")]
+	[SerializeField] private float minRotation;
+	[SerializeField] private float maxRotation;
+
+	[SerializeField] private Transform parentTransform;
+
+	private void OnMouseDrag()
+	{
+		RotatePlatform();
+		RotateParent();
+	}
+
+	private void RotatePlatform()
+	{
+		float dir = EulerToRotation(transform.localEulerAngles.x);
+
+		float rotX = Input.GetAxis("Mouse X") * Mathf.Deg2Rad * rotationSpeed;
+		
+		if (dir < minRotation && rotX < 0)
+		{
+			transform.localEulerAngles = Vector3.right * minRotation;
+			return;
+		}
+
+		if (dir > maxRotation && rotX > 0)
+		{
+			transform.localEulerAngles = Vector3.right * maxRotation;
+			return;
+		}
+
+		transform.Rotate(Vector3.Lerp(transform.localEulerAngles, Vector3.right * rotX * Time.deltaTime, rotationSpeed));
+	}
+
+	private void RotateParent() 
+	{
+		float dir = EulerToRotation(parentTransform.localEulerAngles.z);
+
+		float rotZ = Input.GetAxis("Mouse Y") * Mathf.Deg2Rad * rotationSpeed;
+
+		if (dir < minRotation && rotZ < 0)
+		{
+			parentTransform.localEulerAngles = Vector3.forward * minRotation;
+			return;
+		}
+
+		if (dir > maxRotation && rotZ > 0)
+		{
+			parentTransform.localEulerAngles = Vector3.forward * maxRotation;
+			return;
+		}
+
+		parentTransform.Rotate(Vector3.Lerp(parentTransform.localEulerAngles, Vector3.forward * rotZ * Time.deltaTime, rotationSpeed));
+	}
+
+	private float EulerToRotation(float value)
+	{
+		if (value > 180)
+		{
+			return value - 360f;
+		}
+		else if (value < -180)
+		{
+			return value + 360f;
+		}
+		else
+		{
+			return value;
+		}
+	}
+}
